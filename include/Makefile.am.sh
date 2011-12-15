45da4843bc03dff5cbb68563b28284c52b2c9667
Alejandro Mery <amery@geeks.cl>
2011-12-15 22:31:24 +0100

    build: install sancus_fd.h

--- include/Makefile.am.sh
+++ include/Makefile.am.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+list() {
+	echo "$@" | sort -V | fmt -w60 | tr '\n' '|' |
+		sed -e 's,|$,,' -e 's,|, \\\n\t,g'
+}
+
 cd "${0%/*}"
 cat <<EOT | tee Makefile.am
+include_HEADERS = \\
+	$(list *.h)
 EOT
