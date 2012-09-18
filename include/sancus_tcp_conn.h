71d2f91eeb940aab41e8343a193c40b05b918dd0
Alejandro Mery <amery@geeks.cl>
2012-09-18 20:43:14 +0200

    sancus_tcp_conn: introduced client side tcp connections [WIP]

---
+++ include/sancus_tcp_conn.h
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2012, Alejandro Mery <amery@geeks.cl>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef _SANCUS_TCP_CLIENT_H
+#define _SANCUS_TCP_CLIENT_H
+
+struct sancus_tcp_conn;
+
+enum sancus_tcp_conn_error {
+	SANCUS_TCP_CONN_WATCHER_ERROR,
+};
+enum sancus_tcp_conn_state {
+	SANCUS_TCP_CONN_INPROGRESS,
+	SANCUS_TCP_CONN_CONNECTED,
+};
+
+struct sancus_tcp_conn_settings {
+	void (*on_read) (struct sancus_tcp_conn *,
+			 struct ev_loop *);
+	void (*on_write) (struct sancus_tcp_conn *,
+			 struct ev_loop *);
+	void (*on_error) (struct sancus_tcp_conn *,
+			  struct ev_loop *,
+			  enum sancus_tcp_conn_error);
+};
+
+struct sancus_tcp_conn {
+	struct ev_io io;
+	enum sancus_tcp_conn_state state;
+
+	const struct sancus_tcp_conn_settings *settings;
+};
+
+/**
+ * sancus_tcp_conn_start - start watching connection
+ *
+ * @self:	connection to be started
+ * @loop:	event loop
+ */
+void sancus_tcp_conn_start(struct sancus_tcp_conn *self,
+			   struct ev_loop *loop);
+
+/**
+ * sancus_tcp_conn_stop - stop watching connection
+ *
+ * @self:	connection to be stopped
+ * @loop:	event loop
+ */
+void sancus_tcp_conn_stop(struct sancus_tcp_conn *self,
+			  struct ev_loop *loop);
+
+/**
+ * sancus_tcp_conn_close - closes an already stopped connection
+ *
+ * @self:	connection to close
+ */
+void sancus_tcp_conn_close(struct sancus_tcp_conn *self);
+
+/**
+ * sancus_tcp_ipv4_connect - initialized and prepares an ipv4 tcp connection
+ *
+ * @self:	connection structure
+ *
+ * Returns 0 if @addr is invalid, 1 on success and -1 on error. errno set
+ * accordingly.
+ */
+int sancus_tcp_ipv4_connect(struct sancus_tcp_conn *self,
+			    const struct sancus_tcp_conn_settings *settings,
+			    const char *addr, unsigned port,
+			    bool cloexec);
+
+#endif
---
+++ src/tcp_conn.c
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2011-2012, Alejandro Mery <amery@geeks.cl>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>
+
+#include "sancus_common.h"
+#include "sancus_fd.h"
+#include "sancus_socket.h"
+#include "sancus_tcp_conn.h"
+
+static void io_cb(struct ev_loop *loop, struct ev_io *w, int revents)
+{
+	struct sancus_tcp_conn *self = container_of(w, struct sancus_tcp_conn, io);
+	const struct sancus_tcp_conn_settings *settings = self->settings;
+
+	if (revents & EV_READ) {
+		settings->on_read(self, loop);
+	}
+	if (revents & EV_WRITE) {
+		settings->on_write(self, loop);
+	}
+	if (revents & EV_ERROR) {
+		settings->on_error(self, loop, SANCUS_TCP_CONN_WATCHER_ERROR);
+	}
+}
+
+/*
+ * init helpers
+ */
+static inline int init_ipv4(struct sockaddr_in *sin, const char *addr, unsigned port)
+{
+	sin->sin_port = htons(port);
+
+	return inet_pton(sin->sin_family, addr, &sin->sin_addr);
+}
+
+static inline int init_tcp(struct sancus_tcp_conn *self,
+			   const struct sancus_tcp_conn_settings *settings,
+			   struct sockaddr *sa, socklen_t sa_len,
+			   bool cloexec)
+{
+	int fd = sancus_socket(sa->sa_family, SOCK_STREAM, cloexec, true);
+	if (fd < 0)
+		return -1;
+
+	assert(self);
+	assert(settings);
+
+	ev_io_init(&self->io, io_cb, fd, EV_READ|EV_WRITE);
+
+	self->settings = settings;
+
+	if (connect(fd, sa, sa_len) < 0) {
+		if (errno == EINPROGRESS) {
+			self->state = SANCUS_TCP_CONN_INPROGRESS;
+		} else {
+			int e = errno;
+			sancus_close(&self->io.fd);
+			errno = e;
+			return -1;
+		}
+	} else {
+		self->state = SANCUS_TCP_CONN_CONNECTED;
+	}
+
+	return 1;
+}
+
+/*
+ * exported functions
+ */
+void sancus_tcp_conn_start(struct sancus_tcp_conn *self, struct ev_loop *loop)
+{
+	assert(!ev_is_active(&self->io));
+	ev_io_start(loop, &self->io);
+}
+
+void sancus_tcp_conn_stop(struct sancus_tcp_conn *self, struct ev_loop *loop)
+{
+	assert(ev_is_active(&self->io));
+	ev_io_stop(loop, &self->io);
+}
+
+void sancus_tcp_conn_close(struct sancus_tcp_conn *self)
+{
+	assert(self->io.fd >= 0);
+	assert(!ev_is_active(&self->io));
+
+	sancus_close(&self->io.fd);
+}
+
+int sancus_tcp_ipv4_connect(struct sancus_tcp_conn *self,
+			    const struct sancus_tcp_conn_settings *settings,
+			    const char *addr, unsigned port,
+			    bool cloexec)
+{
+	struct sockaddr_in sin = { .sin_family = AF_INET };
+	int e;
+
+	if ((e = init_ipv4(&sin, addr, port)) != 1)
+		return e;
+
+	return init_tcp(self, settings, (struct sockaddr *)&sin, sizeof(sin),
+			cloexec);
+}
