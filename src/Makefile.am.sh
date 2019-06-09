7502d4f6124da1f1f071830d8a04dd597a296d68
Alejandro Mery <amery@geeks.cl>
2019-06-09 20:24:25 +0100

    buildsys: remove libev.pc usage - not official
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- src/Makefile.am.sh
+++ src/Makefile.am.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
+set -eux
 list() {
 	echo "$@" | tr ' ' '\n' | sort -V | tr '\n' '|' |
 		sed -e 's,|$,,' -e 's,|, \\\n\t,g'
 }
 
+F="${0##*/}"
+F="${F%.sh}"
+
 cd "${0%/*}"
-cat <<EOT | tee Makefile.am
-AM_CFLAGS = -I @top_srcdir@/include @EV_CFLAGS@
+cat <<EOT > $F~
+AM_CFLAGS = -I @top_srcdir@/include
 
 lib_LTLIBRARIES = libsancus-core.la
 
 libsancus_core_la_SOURCES = \\
 	$(list *.c)
-
-libsancus_core_la_LIBADD = @EV_LIBS@
 EOT
+mv $F~ $F
