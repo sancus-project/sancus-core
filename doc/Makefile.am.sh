4f4021062c8058501c64c43de52f4ab65f7de327
Alejandro Mery <amery@geeks.cl>
2019-06-17 18:25:21 +0100

    build-sys: add initial doxygen integration
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- doc/Makefile.am.sh
+++ doc/Makefile.am.sh
@@ -2,4 +2,17 @@
 
 cd "${0%/*}"
 cat <<EOT | tee Makefile.am
+
+TSTAMP = doxyfile.stamp
+
+if HAVE_DOXYGEN
+
+\$(TSTAMP): Doxyfile
+	\$(DOXYGEN) $^
+	touch \$@
+
+CLEANFILES = \$(TSTAMP)
+all-local: \$(TSTAMP)
+
+endif
 EOT
