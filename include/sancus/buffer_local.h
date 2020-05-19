a057421729e0dd91e7f9073941e2f3f39152bf56
Alejandro Mery <amery@linux.com>
2020-05-19 22:50:12 +0100

    buffer: introduce sancus_lbuffer to simplify local usage
    Signed-off-by: Alejandro Mery <amery@linux.com>

---
+++ include/sancus/buffer_local.h
@@ -0,0 +1,28 @@
+#ifndef __SANCUS_BUFFER_LOCAL__
+#define __SANCUS_BUFFER_LOCAL__
+
+#include <sancus/buffer.h>
+
+#define DECL_SANCUS_LBUFFER(N, S) struct { \
+	char buf[S]; \
+	struct sancus_buffer b; \
+} N = { .b = { \
+	.buf = (N).buf, \
+	.size = (S), \
+}, };
+
+/*
+ * accessors
+ */
+#define sancus_lbuffer_to_buffer(B) ((B) != NULL ? &(B)->b : NULL)
+
+#define sancus_lbuffer_ptr(B) sancus_buffer_ptr(sancus_lbuffer_to_buffer(B))
+
+/*
+ * append
+ */
+#define sancus_lbuffer_append(B,  S, N) sancus_buffer_append(sancus_lbuffer_to_buffer(B),  (S), (N))
+#define sancus_lbuffer_appendz(B, S)    sancus_buffer_appendz(sancus_lbuffer_to_buffer(B), (S))
+#define sancus_lbuffer_appendf(B, ...)  sancus_buffer_appendf(sancus_lbuffer_to_buffer(B), __VA_ARGS__)
+
+#endif /* !__SANCUS_BUFFER_LOCAL__ */
