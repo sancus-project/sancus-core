ca1143034e050a106b4a965008af278e254a8476
Alejandro Mery <amery@linux.com>
2020-10-17 23:28:36 +0100

    build-sys: improve Makefile.am generation when the lists only include one item
    Signed-off-by: Alejandro Mery <amery@linux.com>

--- include/Makefile.am.sh
+++ include/Makefile.am.sh
@@ -2,15 +2,19 @@
 
 set -eu
 
+F="${0##*/}"
+F="${F%.sh}"
+BASE="$(dirname "$0")"
+
+echo "Generating $BASE/$F" >&2
+
 list() {
 	sort -V | tr '\n' '|' |
 		sed -e 's,|$,,' -e 's,|, \\\n\t,g'
 }
 
-F="${0##*/}"
-F="${F%.sh}"
+cd "$BASE"
 
-cd "${0%/*}"
 cat <<EOT > $F~
 nobase_include_HEADERS = \\
 	$(find * -name '*.h' | grep -v netlink | list)
--- src/Makefile.am.sh
+++ src/Makefile.am.sh
@@ -1,31 +1,60 @@
 #!/bin/sh
 
 set -eu
-list() {
-	sort -uV | tr '\n' '|' |
-		sed -e 's,|$,,' -e 's,|, \\\n\t,g'
-}
 
 F="${0##*/}"
 F="${F%.sh}"
+BASE="$(dirname "$0")"
 
-cd "${0%/*}"
+echo "Generating $BASE/$F" >&2
+
+list() {
+	sort -uV | tr '\n' '|' |
+		sed -e 's,|$,,' -e 's,|, \\\n\t,g' -e 's|^|\t|'
+}
+
+list_files() {
+	local k="$1"; shift
+	local x=
+
+	if [ $# -gt 1 ]; then
+		cat <<EOT
+$k = \\
+EOT
+		for x; do
+			echo "$x"
+		done | list
+	elif [ $# -eq 1 ]; then
+		cat <<EOT
+$k = $1
+EOT
+	else
+		cat <<EOT
+$k =
+EOT
+	fi
+}
+
+list_find_files() {
+	local k="$1"; shift
+	list_files "$k" $(find "$@" 2> /dev/null)
+}
+
+cd "$BASE"
 cat <<EOT > $F~
 AM_CFLAGS = @WARN_CFLAGS@
 AM_CPPFLAGS = -I @top_srcdir@/include
 
 lib_LTLIBRARIES = libsancus-core.la
 
-libsancus_core_la_SOURCES = \\
-	$(find sancus/ -name '*.c' | list)
+$(list_find_files libsancus_core_la_SOURCES sancus/ -name '*.c')
 
 libsancus_core_la_LDFLAGS = -Wl,--no-undefined
 
 if HAVE_LINUX_NETLINK
 lib_LTLIBRARIES += libsancus-netlink.la
 
-libsancus_netlink_la_SOURCES = \\
-	$(find netlink/ -name '*.c' | list)
+$(list_find_files libsancus_netlink_la_SOURCES netlink/ -name '*.c')
 
 libsancus_netlink_la_LDFLAGS = -Wl,--no-undefined
 endif
@@ -46,7 +75,7 @@ find tests -name '*.c' 2> /dev/null 2> /dev/null | cut -d/ -f2 | sort -uV | whil
 #
 TESTS += $n
 test_PROGRAMS += $n
-${N}_SOURCES = $(find tests/$f -name '*.c' | list)
+$(list_find_files ${N}_SOURCES tests/$f -name '*.c')
 ${N}_LDADD = libsancus-core.la
 EOT
 done
